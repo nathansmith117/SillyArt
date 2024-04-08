2b4eb33812e67e2b5acdbe3709b791b2841f6153
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-04-08 09:43:45 -0600

    Added imports for canvas panel

--- src/art/view/CanvasPanel.java
+++ src/art/view/CanvasPanel.java
@@ -1,6 +1,21 @@
 package art.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+
 import art.controller.Controller;
 
 public class CanvasPanel extends JPanel
