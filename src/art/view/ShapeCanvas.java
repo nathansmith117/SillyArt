1aa04583255d76e7f34f15fbd7ded860d83f5398
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-04-16 08:12:12 -0600

    Making shape canvas

---
+++ src/art/view/ShapeCanvas.java
@@ -0,0 +1,45 @@
+package art.view;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+
+import art.controller.Controller;
+
+public class ShapeCanvas extends JPanel
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private Controller app;
+	
+	private BufferedImage canvasImage;
+	
+	public ShapeCanvas(Controller app)
+	{
+		super();
+		this.app = app;
+		
+		this.triangleList = new ArrayList<Polygon>();
+		this.polygonList = new ArrayList<Polygon>();
+		this.ellipseList = new ArrayList<Ellipse2D>();
+		this.rectangleList = new ArrayList<Rectangle>();
+		
+		canvasImage = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
+	}
+}
