9d8a5fc072e752ab6059c1cc948aa22cf1d36ed6
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-04-22 07:59:17 -0600

    Made add polygon method

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -198,4 +198,28 @@ public class ArtPanel extends JPanel
 		layout.putConstraint(SpringLayout.SOUTH, canvas, 0, SpringLayout.SOUTH, sliderPanel);
 		layout.putConstraint(SpringLayout.WEST, canvas, 5, SpringLayout.EAST, sliderPanel);
 	}
+	
+	private boolean coinFlip()
+	{
+		return (int)(Math.random() * 2) == 0;
+	}
+	
+	private Polygon createPolygon(int sides)
+	{
+		Polygon currentShape = new Polygon();
+		
+		int originX = (int)(Math.random() * 800);
+		int originY = (int)(Math.random() * 800);
+		
+		for (int index = 0; index < sides; index++)
+		{
+			int minus = coinFlip() ? -1 : 1;
+			int shiftX = (int)(Math.random() * currentScale) * minus;
+			minus = coinFlip() ? -1 : 1;
+			int shiftY = (int)(Math.random() * currentScale) * minus;
+			currentShape.addPoint(originX + shiftX, originY + shiftY);
+		}
+		
+		return currentShape;
+	}
 }
